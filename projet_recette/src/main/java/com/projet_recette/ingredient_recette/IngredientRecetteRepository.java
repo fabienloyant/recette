aa7ad697d7b9fc712334477914be5640c5c09cca
estelle <estelle.buguet@hotmail.com>
2023-05-05 11:02:35 +0200

    ajout de ingredientrecette

---
+++ projet_recette/src/main/java/com/projet_recette/ingredient_recette/IngredientRecetteRepository.java
@@ -0,0 +1,7 @@
+package com.projet_recette.ingredient_recette;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IngredientRecetteRepository extends JpaRepository<IngredientRecette, Integer>{
+
+}
