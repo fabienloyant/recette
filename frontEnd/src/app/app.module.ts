a16ff8cf7aba7f71104a16fbf815e0e74725b783
mamisoa <mamisoajems@gmail.com>
2023-05-22 17:17:08 +0200

    formulaire

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConnexionFormComponent } from './layouts/connexion-form/connexion-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnexionFormComponent
   ],
   imports: [
     BrowserModule,
