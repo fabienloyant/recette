874fdc04d24d757f2c9bc376866f7d1185ce0338
fabienloyant <fabienloyant@gmail.com>
2023-05-23 09:34:53 +0200

    page home

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ConnexionFormComponent } from './layouts/connexion-form/connexion-form.
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     ConnexionFormComponent,
     FooterComponent,
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
